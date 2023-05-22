83df23f3dc3c1673faee80e696dcd0fb9343e641
Darlan <dcpoffo@gmail.com>
2023-05-21 21:17:06 -0300

    Alterando pagina inicial

--- frontend/src/i18n/locales/pt-br.ts
+++ frontend/src/i18n/locales/pt-br.ts
@@ -53,7 +53,49 @@ export default {
         password: 'Senha',
         add: 'Cadastrar Participante',
         edit: 'Alterar Participante',
-      }
+      },
+      publicPage: {
+        login: 'Acessar',
+        backToTop: 'Voltar ao topo',
+        slides: {
+          first: {
+            title: 'Formação em desenvolvimento WEB',
+            description: 'O Saper.edu nasceu de uma parceria entre o Instituto Federal de Educação, Ciência e Tecnologia do Ceará e a empresa Philips através de incentivos da Lei de Informática.'
+          },
+          second: {
+            title: 'Mercado de Programação',
+            description: 'Quer começar bem na área de TI? Conheça o projeto Saper.edu e fique por dentro da nossa Formação em Desenvolvimento WEB. Curso gratuito, certificação garantida, aulas ao vivo e online, expertise IFCE'
+          },
+          third: {
+            title: 'Processo Seletivo',
+            description: 'Nosso curso Formação em desenvolvimento WEB tem processo seletivo e vagas limitadas, acompanhem nosso calendário. Vem para o projeto Saper.edu'
+          }
+        },
+        alunos: {
+          viewDetails: 'Detalhes',
+          darlan: {
+            name: 'Darlan',
+            description: 'Aluno do curso de Desenvolvimento WEB'
+          },
+          arthur: {
+            name: 'Arthur',
+            description: 'Aluno do curso de Desenvolvimento WEB'
+          },
+          maryanne: {
+            name: 'Maryanne',
+            description: 'Aluna do curso de Desenvolvimento WEB'
+          },
+          diego: {
+            name: 'Diego',
+            description: 'Aluno do curso de Desenvolvimento WEB'
+          },
+        }
+      },
+      notFound: {
+        title: "Página não encontrada :(",
+        message: "Não encontramos a página que você está procurando, clique no link abaixo para ser redirecionado!",
+        home: 'Voltar para home!'
+      },
     }
   },
 }
